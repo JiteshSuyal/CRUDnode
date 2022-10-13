dac4229741cf4e25d469c305ad3a56aae9a00d44
Jitesh_Suyal <jiteshasuyal@gmail.com>
2022-10-13 10:27:51 +0530

    address trst done fully

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const userRoute = require("./src/routes/user.routes");
-const cors = require("cors");
 
 require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT;
